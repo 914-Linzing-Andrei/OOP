aec62d3dbe3245e594bb7b90db28b7fbd322b68e
Linzing Andrei <107073731+914-Linzing-Andrei@users.noreply.github.com>
2023-05-24 13:25:11 +0300

    Add files via upload

---
+++ Cars/PregatireOOPTest3.cpp
@@ -0,0 +1,63 @@
+#include "PregatireOOPTest3.h"
+
+PregatireOOPTest3::PregatireOOPTest3(QWidget *parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+
+    this->populateList();
+}
+
+PregatireOOPTest3::~PregatireOOPTest3()
+{}
+
+void PregatireOOPTest3::populateList()
+{
+    this->ui.carsListWidget->clear();
+
+    vector<Car> allCars = this->service.showAll();
+    sort(allCars.begin(), allCars.end(), [](Car c1, Car c2) {
+        return c1.getManufacturer() < c2.getManufacturer();
+        });
+    for (auto p : allCars) {
+        this->ui.carsListWidget->addItem(QString::fromStdString(p.getManufacturer() + " | " + p.getModel()));
+    }
+}
+
+void PregatireOOPTest3::ShowCarsWithFont() {
+    this->ui.carsListWidget->clear();
+
+    vector<Car> allCars = this->service.showAll();
+    for (auto p : allCars) {
+        QString itemText = QString::fromStdString(p.getManufacturer() + " | " + p.getModel() + " | " + to_string(p.getYear()) + " | " + p.getColour());
+        QListWidgetItem* item = new QListWidgetItem(itemText);
+        QColor fontColor = QColor(QString::fromStdString(p.getColour()));
+        item->setForeground(QBrush(fontColor));
+        this->ui.carsListWidget->addItem(item);
+    }
+}
+
+void PregatireOOPTest3::showCarsWithSpecificManufacturer() {
+    this->ui.carsWithSpecificManufacturerlistWidget->clear();
+    vector<Car> allCars = this->service.showAll();
+    string manufacturer = this->ui.manufacturerlineEdit->text().toStdString();
+    
+    int numberOfCarsWithGivenManufacturer = count_if(allCars.begin(), allCars.end(), [&](Car car) {
+        return car.getManufacturer() == manufacturer;
+        }
+    );
+    string number = to_string(numberOfCarsWithGivenManufacturer);
+    if(number != "0")
+        this->ui.carsWithSpecificManufacturerlistWidget->addItem(QString::fromStdString(number));
+
+    for (auto p : allCars) {
+        if (p.getManufacturer() == manufacturer) {
+            this->ui.carsWithSpecificManufacturerlistWidget->addItem(
+                QString::fromStdString(p.getManufacturer() + " | " + 
+                                       p.getModel() + " | " + 
+                                       to_string(p.getYear()) + " | " +
+                                       p.getColour())
+                );
+        }
+    }
+}
